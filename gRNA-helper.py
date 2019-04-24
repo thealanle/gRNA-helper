71de7b822c646287693a1829756016205f7b929d
alan <35761977+thealanle@users.noreply.github.com>
2019-04-23 22:19:13 -0700

    Add FASTA file reading ability

--- gRNA-helper.py
+++ gRNA-helper.py
@@ -1,3 +1,12 @@
-def read_fasta(filename):
-    with open(filename, 'r') as fasta_file:
-        return "".join([line.strip() for line in fasta_file])[1:]
+def read_fasta(filepath):
+    """
+    Given a path to a FASTA file, return the header and sequence.
+    """
+    with open(filepath, 'r') as fasta_file:
+        header = fasta_file.readline().strip()
+        sequence = "".join([line.strip()
+                            for line in fasta_file.readlines()])
+    return (header, sequence)
+
+
+print(read_fasta('mecA_fasta.txt'))
